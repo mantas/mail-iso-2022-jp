cfeb197eac1667fa52882c2f33ba341136c8624d
Tsutomu Kuroda <t-kuroda@oiax.jp>
2013-04-14 01:45:47 +0900

    Do not encode field value if value is ascii only

--- lib/mail-iso-2022-jp/field.rb
+++ lib/mail-iso-2022-jp/field.rb
@@ -10,16 +10,11 @@ module Mail
           raise ::Mail::InvalidEncodingError.new(
             "The '#{name}' field is not encoded in UTF-8 nor in US-ASCII but in #{value.encoding}")
         end
+        charset = 'utf-8' if value.ascii_only?
       end
-      charset = 'utf-8' if already_encoded?(name, charset)
       initialize_without_iso_2022_jp_encoding(name, value, charset)
     end
     alias_method :initialize_without_iso_2022_jp_encoding, :initialize
     alias_method :initialize, :initialize_with_iso_2022_jp_encoding
-
-    private
-    def already_encoded?(name, charset)
-      charset == 'ISO-2022-JP' && name =~ /(^Subject:\ \=\?ISO-2022-JP\?)/
-    end
   end
 end
